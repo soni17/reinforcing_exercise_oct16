d4b4042d585f7fe055b43ece49035f607b494833
soni17 <elb004@gmail.com>
2017-10-16 11:30:00 -0400

    simplified if statements

--- exercise.rb
+++ exercise.rb
@@ -4,12 +4,12 @@ counter = 1
 
 def calcNum(arg)
 
-  if arg%2 == 0 && arg%7 != 0
-    return arg+1
-  elsif arg%7 == 0 && arg%2 != 0
-    return arg-1
-  elsif arg%2 == 0 && arg%7 == 0
+  if arg%2 == 0 && arg%7 == 0
     return arg*2
+  elsif arg%2 == 0
+    return arg+1
+  elsif arg%7 == 0
+    return arg-1
   else
     return arg
   end
